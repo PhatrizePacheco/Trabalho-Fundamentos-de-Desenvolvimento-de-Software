beb9a13465d6a7a7c93c22ddeaefe132c10b4e3f
Phatrize <phatrize13@gmail.com>
2025-07-02 01:30:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+    const botao = document.getElementById('botao');
+    const mensagem = document.getElementById('mensagem');
+
+    
+    botao.addEventListener('click', e =>{
+        mensagem.innerHTML="Quanto maior o risco, maior a recompensa.";
+    });
+
+
+
+
+
+
+
+
